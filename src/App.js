ec6d59f08790d6ae0e4e6ffd17c042de62439a26
Jackson Booth <jacksontbooth@icloud.com>
2022-02-14 16:16:06 +1100

    added api call ;)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 
 import InputBar from './components/inputbar';
 
+const BASE_URL = 'https://api.exchangeratesapi.io/v1/latest'
+
 function App() {
+
+ useEffect(() => {
+   fetch(BASE_URL)
+   .then(res => res.json())
+   .then(data => console.log(data))
+ }, [])
+
   return (
     <div className="App">
         <InputBar />
